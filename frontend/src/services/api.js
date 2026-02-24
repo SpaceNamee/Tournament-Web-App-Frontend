7114133f644c6cfc8be5c7867dcbbad40154eac3
Marianna <mariannabenkalovych@gmail.com>
2026-02-24 23:36:39 +0100

    Add withCredentials: true

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://tournament-web-app-backend-1.onrender.com",
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
